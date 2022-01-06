0e0b5cfdf0fcf91b5d238d670e424e980ec17817
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-01-06 03:48:17 +0300

    add declaring variables

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function akanNames(e) {
+    e.preventDefault();
+
+    var cc=document.getElementById('century').value;
+    var cc=document.getElementById('year').value;
+    var cc=document.getElementById('month').value;
+    var cc=document.getElementById('date').value;
+    var cc=document.getElementById('dot-1').value;
+    var cc=document.getElementById('dot=2').value;
+}
+}
+
+
+
+
