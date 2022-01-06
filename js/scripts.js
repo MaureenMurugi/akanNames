c6906ea7e204f74e6170cb95c36c3ff5e08e6e9e
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-01-06 03:58:15 +0300

    add varibles and validation of variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,35 @@
 function akanNames(e) {
     e.preventDefault();
+// Variable declaration
 
     var cc=document.getElementById('century').value;
-    var cc=document.getElementById('year').value;
-    var cc=document.getElementById('month').value;
-    var cc=document.getElementById('date').value;
-    var cc=document.getElementById('dot-1').value;
-    var cc=document.getElementById('dot=2').value;
+    var yy=document.getElementById('year').value;
+    var mm=document.getElementById('month').value;
+    var dd=document.getElementById('date').value;
+    var gender=document.getElementById('dot-1').value;
+    var gender=document.getElementById('dot-2').value;
+// Validation
+
+    if(cc ==''|| cc<19||cc>21) {
+        alert('Input a valid century');
+        return false;
+    }
+    if(yy ==''|| yy.length>4 || yy.length<4) {
+        alert('Input valid year');
+        return false;
+    }
+    if(mm ==''|| mm<=0 || mm>12) {
+        alert('Input a valid month');
+        return false;
+    }
+    if(dd == ''|| dd<=0 || dd>31) {
+        alert('Input a valid day');
+        return false;
+    }
+
+
 }
-}
+
 
 
 
